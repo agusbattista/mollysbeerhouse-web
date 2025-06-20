1a0df7060b39e6e088cd2d425bb55d3e33d064df
agusbattista <130807422+agusbattista@users.noreply.github.com>
2025-06-20 00:25:13 -0300

    refactoring: usar async/await en formulario

--- scripts/Formulario.js
+++ scripts/Formulario.js
@@ -9,34 +9,33 @@ class Formulario {
   }
 
   #configurarEventos = () => {
-    this.#formulario.addEventListener("submit", (evento) => {
+    this.#formulario.addEventListener("submit", async (evento) => {
       evento.preventDefault();
-      this.#validarYEnviarFormulario();
+      await this.#validarYEnviarFormulario();
     });
   };
 
-  #validarYEnviarFormulario = () => {
+  #validarYEnviarFormulario = async () => {
     if (this.#formulario.checkValidity()) {
       this.#alerta.alertCargando();
-      fetch(this.#formulario.action, {
-        method: this.#formulario.method,
-        body: new FormData(this.#formulario),
-        headers: {
-          Accept: "application/json",
-        },
-      })
-        .then((response) => {
-          if (response.ok) {
-            this.#formulario.reset();
-            this.#alerta.alertExito();
-          } else {
-            this.#alerta.alertError();
-          }
-        })
-        .catch((error) => {
-          console.log(error.message);
-          this.#alerta.alertError();
+      try {
+        const response = await fetch(this.#formulario.action, {
+          method: this.#formulario.method,
+          body: new FormData(this.#formulario),
+          headers: {
+            Accept: "application/json",
+          },
         });
+        if (response.ok) {
+          this.#formulario.reset();
+          this.#alerta.alertExito();
+        } else {
+          this.#alerta.alertError();
+        }
+      } catch (error) {
+        console.error("Error al enviar formulario.", error);
+        this.#alerta.alertError();
+      }
     } else {
       this.#formulario.reportValidity();
     }
